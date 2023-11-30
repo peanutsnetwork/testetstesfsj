4bd9154316629caa91387dc723631c865cfcac76
peanutsnetwork <150980628+peanutsnetwork@users.noreply.github.com>
2023-11-30 17:27:11 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+// Endpoint to fetch web pages
+app.get('/fetch-page', async (req, res) => {
+  try {
+    const { url } = req.query;
+    const response = await axios.get(url);
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
